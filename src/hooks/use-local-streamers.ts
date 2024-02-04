5cddc6ff1f14f25d5ab4be34a6f7c281c6852a5e
Luna Santos <lunafeyli@gmail.com>
2024-02-03 21:30:40 -0300

    :bug: Y bueno

--- src/hooks/use-local-streamers.ts
+++ src/hooks/use-local-streamers.ts
@@ -1,21 +1,20 @@
 import { STREAMERS } from "@/data/streamers";
 import { useCustomDataStore } from "@/stores/custom-data-store";
 import { StreamerSchema } from "@/types/streamer.schema";
-import { useSearchParamsData } from "./use-search-params-data";
 import useStore from "./use-store";
 
 export function useLocalStreamers() {
   const customData = useStore(useCustomDataStore, (state) => state);
-  const { streamers: streamersOnQuery } = useSearchParamsData();
+  // const { streamers: streamersOnQuery } = useSearchParamsData();
 
   const Streamers: StreamerSchema[] = [
     customData?.pinnedStreamers || [],
-    streamersOnQuery.filter(
-      (streamer) =>
-        ![...STREAMERS, ...(customData?.pinnedStreamers ?? [])].some(
-          (str) => str.twitch_name === streamer.twitch_name
-        )
-    ),
+    // streamersOnQuery.filter(
+    //   (streamer) =>
+    //     ![...STREAMERS, ...(customData?.pinnedStreamers ?? [])].some(
+    //       (str) => str.twitch_name === streamer.twitch_name
+    //     )
+    // ),
     STREAMERS,
   ].flat();
 
