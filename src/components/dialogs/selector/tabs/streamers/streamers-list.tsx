138284678bc6d0d298b34874dba12159663e9ee5
Luna Santos <lunafeyli@gmail.com>
2024-02-10 15:48:25 -0300

    :lipstick: .

--- src/components/dialogs/selector/tabs/streamers/streamers-list.tsx
+++ src/components/dialogs/selector/tabs/streamers/streamers-list.tsx
@@ -47,7 +47,10 @@ export function StreamersList({
   const sort = useSortStreamers();
 
   return (
-    <ScrollArea className="h-[15.5rem] sm:h-[24.5rem]" type="always">
+    <ScrollArea
+      className="h-[39dvh] sm:h-[47.5dvh]"
+      /*h-[15.5rem] sm:h-[24.5rem]*/ type="always"
+    >
       <ToggleGroup
         type="multiple"
         className="flex-wrap gap-3 py-2"
