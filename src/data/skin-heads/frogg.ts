b485940ecd9e46e0e405ae707101433bc83f3b9c
feyli <48140698+feeeyli@users.noreply.github.com>
2024-05-03 17:28:37 -0300

    .

--- src/data/skin-heads/frogg.ts
+++ src/data/skin-heads/frogg.ts
@@ -1,29 +1,23 @@
 /* cSpell:disable */
 
 export const FROGG_SKIN_HEADS: { [skin: string]: string } = {
-  scottonauta:
-    "https://crafatar.com/avatars/46fa9dec-08a2-4722-bd59-00f190cab20a",
-  umildlive:
-    "https://crafatar.com/avatars/f6a94a1e-9fc9-42b0-bc87-0ac8f4866590",
-  tiba041: "https://crafatar.com/avatars/703196c7-4c8d-4c22-bde8-601a08498a9d",
-  kellerzons:
-    "https://crafatar.com/avatars/5775b22b-b75b-432a-b1a2-29d3d5ce7448",
-  oakinoo: "https://crafatar.com/avatars/87720615-8a89-41af-9434-f32aa0416b86",
-  ameizim: "https://crafatar.com/avatars/f084ae9b-c667-491d-8729-424c048c8076",
-  bastet: "https://crafatar.com/avatars/70999ec8-234b-4ae6-bfb3-0304b698ea07",
-  carrasquera:
-    "https://crafatar.com/avatars/ea00118d-fed9-4300-9bae-d1bb0cd83648",
-  dreasbro: "https://crafatar.com/avatars/e72ad480-fbde-4501-98bb-e2da9579b045",
-  febatista:
-    "https://crafatar.com/avatars/b0bb3c43-de47-4013-bc7b-996f4819d2c7",
-  fehdubs: "https://crafatar.com/avatars/79c50e8f-32a2-452d-89c6-f2574149f63f",
-  oflopi: "https://crafatar.com/avatars/9fb2c3ae-5bab-4773-8cf2-5ece58d65748",
-  jinkiwinkki:
-    "https://crafatar.com/avatars/57d98d0a-2ee3-47e2-ae14-eb32185bc45e",
-  kaaory: "https://crafatar.com/avatars/58bc10e6-93b1-4b5a-b936-cf43248c7af2",
-  kojjlul: "https://crafatar.com/avatars/46b47da6-fd09-4be9-beba-d64717eadb7f",
-  ljoga: "https://crafatar.com/avatars/180ac11e-7beb-4322-9a55-17c26293723d",
-  mynluvsx: "https://crafatar.com/avatars/79c29788-6a07-440e-95ce-ccc63a0644f6",
-  imrafly: "https://crafatar.com/avatars/aab947c7-307a-4171-9f98-19ef68379c49",
-  yuyusaz: "https://crafatar.com/avatars/8d04f1c4-08f1-4d88-bc86-8ac55b55c314",
+  scottonauta: "https://crafatar.com/avatars/46fa9dec-08a2-4722-bd59-00f190cab20a?overlay",
+  umildlive: "https://crafatar.com/avatars/f6a94a1e-9fc9-42b0-bc87-0ac8f4866590?overlay",
+  tiba041: "https://crafatar.com/avatars/703196c7-4c8d-4c22-bde8-601a08498a9d?overlay",
+  kellerzons: "https://crafatar.com/avatars/5775b22b-b75b-432a-b1a2-29d3d5ce7448?overlay",
+  oakinoo: "https://crafatar.com/avatars/87720615-8a89-41af-9434-f32aa0416b86?overlay",
+  ameizim: "https://crafatar.com/avatars/f084ae9b-c667-491d-8729-424c048c8076?overlay",
+  bastet: "https://crafatar.com/avatars/70999ec8-234b-4ae6-bfb3-0304b698ea07?overlay",
+  carrasquera: "https://crafatar.com/avatars/ea00118d-fed9-4300-9bae-d1bb0cd83648?overlay",
+  dreasbro: "https://crafatar.com/avatars/e72ad480-fbde-4501-98bb-e2da9579b045?overlay",
+  febatista: "https://crafatar.com/avatars/b0bb3c43-de47-4013-bc7b-996f4819d2c7?overlay",
+  fehdubs: "https://crafatar.com/avatars/79c50e8f-32a2-452d-89c6-f2574149f63f?overlay",
+  oflopi: "https://crafatar.com/avatars/9fb2c3ae-5bab-4773-8cf2-5ece58d65748?overlay",
+  jinkiwinkki: "https://crafatar.com/avatars/57d98d0a-2ee3-47e2-ae14-eb32185bc45e?overlay",
+  kaaory: "https://crafatar.com/avatars/58bc10e6-93b1-4b5a-b936-cf43248c7af2?overlay",
+  kojjlul: "https://crafatar.com/avatars/46b47da6-fd09-4be9-beba-d64717eadb7f?overlay",
+  ljoga: "https://crafatar.com/avatars/180ac11e-7beb-4322-9a55-17c26293723d?overlay",
+  mynluvsx: "https://crafatar.com/avatars/79c29788-6a07-440e-95ce-ccc63a0644f6?overlay",
+  imrafly: "https://crafatar.com/avatars/aab947c7-307a-4171-9f98-19ef68379c49?overlay",
+  yuyusaz: "https://crafatar.com/avatars/8d04f1c4-08f1-4d88-bc86-8ac55b55c314?overlay",
 };
