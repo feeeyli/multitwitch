d0fff13dc3ac898860829532efd739bd0bbc3ba0
feyli <48140698+feeeyli@users.noreply.github.com>
2025-01-17 18:53:52 -0300

    Update frogg.ts

--- src/data/streamers/frogg.ts
+++ src/data/streamers/frogg.ts
@@ -3,189 +3,212 @@
 import { StreamerSchema } from "@/types/streamer.schema";
 
 export const FROGG_STREAMERS: StreamerSchema[] = [
-  {
-    display_name: "Scott",
-    twitch_name: "scottonauta",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/1917209e-3c4e-49c2-8c09-e4e77141e1e1-profile_image-300x300.png",
-  },
-  {
-    display_name: "Umild",
-    twitch_name: "umildlive",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/fa41da80-e980-4bb6-a580-bd6ea487b14f-profile_image-300x300.png",
-  },
-  {
-    display_name: "Tiba",
-    twitch_name: "tiba041",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/89dba2f2-567d-4697-840f-1c3cc863e7f0-profile_image-300x300.png",
-  },
-  {
-    display_name: "Keller",
-    twitch_name: "kellerzons",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/8ebe8da4-c266-4809-92bb-605331a29627-profile_image-300x300.png",
-  },
-  {
-    display_name: "Akino",
-    twitch_name: "oakinoo",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/441d9598-0dc8-4dc9-bad0-e5b03e017ffb-profile_image-300x300.png",
-  },
-  {
-    display_name: "Ameizim",
-    twitch_name: "ameizim",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/69a4e8bc-01be-4f11-95ab-1c949932a13a-profile_image-300x300.png",
-  },
-  {
-    display_name: "Amelie",
-    twitch_name: "ameliebluie",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/c6b18c74-54c8-42bc-85e0-ca199397acaf-profile_image-300x300.png",
-  },
-  {
-    display_name: "Bastet",
-    twitch_name: "bastet",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/fee53afc-157d-412e-8761-6f2f3dd741ef-profile_image-300x300.png",
-  },
-  {
-    display_name: "Carras",
-    twitch_name: "carrasquera",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/7b4253a0-eb8a-4f5e-8ea2-1b715c6c39b7-profile_image-300x300.png",
-  },
-  {
-    display_name: "Dreas",
-    twitch_name: "dreasbro",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/b2784187-3ee7-4c31-8161-59ac6fddda42-profile_image-300x300.png",
-  },
-  {
-    display_name: "Febatista",
-    twitch_name: "febatista",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/0129c7d8-8e76-4501-8bad-35e6c0d3c0f6-profile_image-300x300.png",
-  },
-  {
-    display_name: "FehDubs",
-    twitch_name: "fehdubs",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/f2614f03-9849-478f-8110-ab9adf8a8531-profile_image-300x300.png",
-  },
-  {
-    display_name: "Flopi",
-    twitch_name: "oflopi",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/23d2b93f-4e22-4724-a494-266310c8fd71-profile_image-300x300.png",
-  },
-  {
-    display_name: "Guaxinim",
-    twitch_name: "guaxinim",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/3a88fb53-3d0b-45f5-8658-6bb6b49575d4-profile_image-300x300.png",
-  },
-  {
-    display_name: "Jinki",
-    twitch_name: "jinkiwinkki",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/e08bd224-64d0-4f1f-8473-1ff7b1dcf195-profile_image-300x300.png",
-  },
-  {
-    display_name: "Kaory",
-    twitch_name: "kaaory",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/4faa84a8-2b12-4123-9b0e-61d471cacf8b-profile_image-300x300.png",
-  },
-  {
-    display_name: "Kojj",
-    twitch_name: "kojjlul",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/031207fe-1ebb-4d1f-b406-27553e0ecb83-profile_image-300x300.png",
-  },
-  {
-    display_name: "LJoga",
-    twitch_name: "ljoga",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/5d465f44-69bf-4b06-b49f-b20944fe0bcf-profile_image-300x300.png",
-  },
-  {
-    display_name: "Mii Reis",
+	{
+		display_name: "Scott",
+		twitch_name: "scott",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/1917209e-3c4e-49c2-8c09-e4e77141e1e1-profile_image-300x300.png",
+	},
+	{
+		display_name: "Umild",
+		twitch_name: "umildlive",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/6bf8fadb-69d5-4708-8827-83cbaaa454dd-profile_image-300x300.png",
+	},
+	{
+		display_name: "Tiba",
+		twitch_name: "tiba041",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/89dba2f2-567d-4697-840f-1c3cc863e7f0-profile_image-300x300.png",
+	},
+	{
+		display_name: "Keller",
+		twitch_name: "kellerzons",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/8ebe8da4-c266-4809-92bb-605331a29627-profile_image-300x300.png",
+	},
+	{
+		display_name: "Akino",
+		twitch_name: "oakinoo",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/d8be8432-f540-451c-8697-8867781efb09-profile_image-300x300.png",
+	},
+	{
+		display_name: "Ameizim",
+		twitch_name: "ameizim",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/69a4e8bc-01be-4f11-95ab-1c949932a13a-profile_image-300x300.png",
+	},
+	{
+		display_name: "Amelie",
+		twitch_name: "ameliebluie",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/c6b18c74-54c8-42bc-85e0-ca199397acaf-profile_image-300x300.png",
+	},
+	{
+		display_name: "Bastet",
+		twitch_name: "bastet",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/fee53afc-157d-412e-8761-6f2f3dd741ef-profile_image-300x300.png",
+	},
+	{
+		display_name: "Carrasquera",
+		twitch_name: "carrasquera",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/ac7035cf-f26e-4fe7-8e91-28d174478ef8-profile_image-300x300.png",
+	},
+	{
+		display_name: "Dreas",
+		twitch_name: "dreasbro",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/b2784187-3ee7-4c31-8161-59ac6fddda42-profile_image-300x300.png",
+	},
+	{
+		display_name: "Febatista",
+		twitch_name: "febatista",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/0129c7d8-8e76-4501-8bad-35e6c0d3c0f6-profile_image-300x300.png",
+	},
+	{
+		display_name: "Feh",
+		twitch_name: "fehdubs",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/f2614f03-9849-478f-8110-ab9adf8a8531-profile_image-300x300.png",
+	},
+	{
+		display_name: "Flopi",
+		twitch_name: "oflopi",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/23d2b93f-4e22-4724-a494-266310c8fd71-profile_image-300x300.png",
+	},
+	{
+		display_name: "Guaxinim",
+		twitch_name: "guaxinim",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/3a88fb53-3d0b-45f5-8658-6bb6b49575d4-profile_image-300x300.png",
+	},
+	{
+		display_name: "Jinki",
+		twitch_name: "jinkiwinkki",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/e08bd224-64d0-4f1f-8473-1ff7b1dcf195-profile_image-300x300.png",
+	},
+	{
+		display_name: "Kaory",
+		twitch_name: "kaaory",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/32a42cae-51c1-4e08-96ee-262bee8729a4-profile_image-300x300.png",
+	},
+	{
+		display_name: "Kojj",
+		twitch_name: "kojjlul",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/1ffb6086-5867-4e1c-8c30-1f6fc1b27600-profile_image-300x300.png",
+	},
+	{
+		display_name: "LJoga",
+		twitch_name: "ljoga",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/8a5c0969-7d8d-4989-944e-6404ffb54ff6-profile_image-300x300.png",
+	},
+	{
+		display_name: "Mii Reis",
     twitch_name: "mii_reis",
-    default_streamer: true,
-    no_data: true,
+		default_streamer: true,
+		no_data: true,
     youtube_stream: true,
-    avatar_url:
-      "https://yt3.googleusercontent.com/GQxO4T8NxrFomToU_u-zy3VSdAi7MueSWWyi9N_lakRVCQVyJloAAZrr4oec9Fr2I7XH5q02=s176-c-k-c0x00ffffff-no-rj",
-  },
-  {
-    display_name: "Myn",
-    twitch_name: "mynluvsx",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/7cee8027-2e00-4c58-924f-7bc30bf90e0c-profile_image-300x300.png",
-  },
-  {
-    display_name: "Rafly",
-    twitch_name: "imrafly",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/41d41923-67ac-4498-9df5-e4d38f46f96f-profile_image-300x300.png",
-  },
-  {
-    display_name: "Yui",
-    twitch_name: "yuiboboca",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/dd054b79-a741-484f-be2c-5ad2f0247c66-profile_image-300x300.png",
-  },
-  {
-    display_name: "Yumi",
-    twitch_name: "yuyusaz",
-    default_streamer: true,
-    no_data: true,
-    avatar_url:
-      "https://static-cdn.jtvnw.net/jtv_user_pictures/634e48a5-bcbb-408e-81ba-b2b7666931f5-profile_image-300x300.png",
-  },
+		avatar_url: "https://yt3.googleusercontent.com/1K1ay068QMysTvz1f_EnshPbfkhndTXti_tCgMjgr7USTQNUY6JytjNTg-QcFa9DWtp3jNnW=s160-c-k-c0x00ffffff-no-rj",
+	},
+	{
+		display_name: "Myn",
+		twitch_name: "mynluvsx",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/e583a10d-d97f-447c-a248-bc946ce056fe-profile_image-300x300.png",
+	},
+	{
+		display_name: "Rafly",
+		twitch_name: "imrafly",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/87229533-a7b8-4303-8894-632803a9cff2-profile_image-300x300.png",
+	},
+	{
+		display_name: "Sunny",
+		twitch_name: "sunnyseiki",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/54d3f43c-c1d6-4302-b87a-5facab31d758-profile_image-300x300.png",
+	},
+	{
+		display_name: "Yna",
+		twitch_name: "ynasshi",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/ab736922-7adc-4a5c-bb8e-33351b8df580-profile_image-300x300.png",
+	},
+	{
+		display_name: "Yume",
+		twitch_name: "yumematsu",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/15ce7641-9eed-480e-b235-faeed4f98714-profile_image-300x300.png",
+	},
+	{
+		display_name: "Yui",
+		twitch_name: "yuiboboca",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/dd054b79-a741-484f-be2c-5ad2f0247c66-profile_image-300x300.png",
+	},
+	{
+		display_name: "Yumi",
+		twitch_name: "yuyusaz",
+		default_streamer: true,
+		no_data: true,
+		avatar_url:
+			"https://static-cdn.jtvnw.net/jtv_user_pictures/634e48a5-bcbb-408e-81ba-b2b7666931f5-profile_image-300x300.png",
+	},
 ];
