78d9d5cd205a9ab4a13bbccd56332939eba07314
feyli <48140698+feeeyli@users.noreply.github.com>
2025-01-24 13:13:01 -0300

    .

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,3 @@ import { NextRequest, NextResponse } from "next/server";
 export default async function middleware(request: NextRequest) {
   return NextResponse.rewrite(new URL("https://froggsmp.vercel.app/multi"));
 }
-
-export const config = {
-  matcher: ["/*"],
-};
